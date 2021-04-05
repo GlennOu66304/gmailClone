009d8082206d30d852b4030d8078ef70f2e52701
Glenn Ou <2738328919@qq.com>
2021-04-05 10:25:59 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/layout/Header'
+import Main from './components/layout/Main'
+function App() {
+  return (
+    <div >
+     <Header />
+     <Main />
+    </div>
+  );
+}
+
+export default App;
