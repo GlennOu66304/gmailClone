ccfc7ef4192ff2772e0dd48aaeff4edbb0da99c1
Glenn Ou <2738328919@qq.com>
2021-04-05 10:30:14 +0800

    Add files via upload

---
+++ src/data/BottomIconData.js
@@ -0,0 +1,10 @@
+import PersonIcon from '@material-ui/icons/Person';
+import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
+import CallIcon from '@material-ui/icons/Call';
+
+export const bottonIcons = [
+    <PersonIcon />,
+    < FormatQuoteIcon />,
+    <CallIcon />
+
+]
